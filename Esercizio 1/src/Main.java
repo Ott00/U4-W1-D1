e2aa7bfca55683366a7e6e67641a0a82c1d56799
Otman Karim <71219931+Ott00@users.noreply.github.com>
2024-01-08 17:48:17 +0100

    Edited first ex.

--- Esercizio 1/src/Main.java
+++ Esercizio 1/src/Main.java
@@ -1,8 +1,13 @@
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("1) " +  Multiply(5,6));
         System.out.println("2) " +  Concatenate("Hello","World"));
+
+        String[] array = AddInArray(new String[]{"A","B","C","E","F"}, "D");
+        System.out.println("Array modificato" + Arrays.toString(array));
     }
     public static int Multiply(int a, int b){
         return a*b;
@@ -11,4 +16,15 @@ public class Main {
         return a+" "+b;
     }
 
+    public static String[] AddInArray(String[] stringArray, String stringToAdd){
+        System.out.println("Array premodifiche" + Arrays.toString(stringArray));
+        String[] newArray = new String[6];
+        newArray[0] = stringArray[0];
+        newArray[1] = stringArray[1];
+        newArray[2] = stringArray[2];
+        newArray[3] = stringToAdd;
+        newArray[4] = stringArray[3];
+        newArray[5] = stringArray[4];
+        return newArray;
+    }
 }
